87b700dc174eb7764fb547f267158712bc9faafb
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-11-19 03:09:18 -0800

    .

--- monty.c
+++ monty.c
@@ -8,26 +8,20 @@
  */
 int main(int argc, char *argv[])
 {
-    FILE *file;
     char *line = NULL;
     stack_t *stack = NULL;
     unsigned int line_number = 0;
     char *token;
+    size_t len = 0;
 
-    if (argc != 2)
+    (void)argv;
+    if (argc != 1)
     {
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
 
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-
-    while (fgets(line, MAX_LINE_LENGTH, file) != NULL)
+    while (getline(&line, &len, stdin) != -1)
     {
         line_number++;
 
@@ -67,7 +61,6 @@ int main(int argc, char *argv[])
         }
     }
 
-    fclose(file);
     if (line)
         free(line);
     /* Free the stack and any other dynamically allocated memory */
