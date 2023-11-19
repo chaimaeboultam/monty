2faef3d6fb5c6a5ec0dd07868dbdb118d759e39a
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-11-19 03:17:12 -0800

    .

--- monty.c
+++ monty.c
@@ -13,16 +13,17 @@ int main(int argc, char *argv[])
     stack_t *stack = NULL;
     unsigned int line_number = 0;
     char *token;
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
 
     if (argc != 2)
     {
         fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
