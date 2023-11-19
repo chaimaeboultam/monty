5f69ec3975c76a1dd2f9b9e4e826bff0bd79de45
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-11-19 02:43:55 -0800

    .

--- monty.c
+++ monty.c
@@ -52,6 +52,10 @@ int main(int argc, char *argv[])
         {
             pall(&stack, line_number);
         }
+	else if (strcmp(token, "pint") == 0)
+        {
+            pint(&stack, line_number);
+        }
         else
         {
             fprintf(stderr, "L%d: unknown instruction %s\n", line_number, token);
--- opcode.c
+++ opcode.c
@@ -52,3 +52,18 @@ void pall(stack_t **stack, unsigned int line_number)
 		current = current->next;
 	}
 }
+/**
+ * pint -  prints the value at the top of the stack
+ * @stack: Double pointer to the beginning of the stack
+ * @line_number: Line number in the Monty bytecode file
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+    {
+        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%d\n", (*stack)->n);
+}
