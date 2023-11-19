23af3a3f76a9a02cc72e2a5f11356c0fd5ac538a
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-11-19 02:04:47 -0800

    .

--- push.c
+++ push.c
@@ -5,7 +5,7 @@
  * @stack: Double pointer to the beginning of the stack
  * @value: Value to be pushed onto the stack
  */
-void push(stack_t **stack, unsigned int value)
+void push(stack_t **stack, int value)
 {
     stack_t *new_node;
 
@@ -33,24 +33,4 @@ void push(stack_t **stack, unsigned int value)
     }
 
     *stack = new_node;
-
 }
-/**
- * pall - Prints all the values on the stack
- * @stack: Double pointer to the beginning of the stack
- * @line_number: Line number in the Monty bytecode file
- */
-void pall(stack_t **stack, unsigned int line_number)
-{
-    stack_t *current = *stack;
-
-    (void)line_number; /* Unused parameter, suppress compiler warning */
-
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
-}
-
-   
